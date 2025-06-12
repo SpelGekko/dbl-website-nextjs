5dcc9ff4163089d675e8fad461fe7572f9078509
SpelGekko <spelgekko@gmail.com>
2025-06-12 17:14:54 +0200

    change default port to 9000

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+  },
+  experimental: {},
+  async rewrites() {
+    return [];
+  },
 };
 
 export default nextConfig;
